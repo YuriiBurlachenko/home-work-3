4a91eab3cbba9e16c981bb43239ebd0479dc3a16
YuriiBurlachenko <burlachenkoyurii@gmail.com>
2023-04-15 00:22:17 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let num1 = Number(prompt('input number 1 '));
+let num2 = Number(prompt('input number 2 '));
+alert(
+  `  ${num1} + ${num2}= ${num1 + num2}
+  ${num1} - ${num2}= ${num1 - num2}
+  ${num1} * ${num2}= ${num1 * num2}
+  ${num1} / ${num2}= ${num1 / num2}`
+);
